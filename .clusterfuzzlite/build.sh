b6b8315b604460840810d77f98d0d97d4f644faa
Oleg Fokin <oleg.fokin@harmonicinc.com>
2022-01-26 10:12:43 +0200

    cflite

---
+++ .clusterfuzzlite/build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -eu
+
+# build project
+# e.g.
+# ./autogen.sh
+# ./configure
+# make -j$(nproc) all
+
+# build fuzzers
+# e.g.
+# $CXX $CXXFLAGS -std=c++11 -Iinclude \
+#     /path/to/name_of_fuzzer.cc -o $OUT/name_of_fuzzer \
+#     $LIB_FUZZING_ENGINE /path/to/library.a
+
+#$CXX $CXXFLAGS $SRC -o $OUT
+clang++ -std=c++17 -stdlib=libstdc++ -g -fsanitize=address,fuzzer fuzz.cpp -o fuzzer
