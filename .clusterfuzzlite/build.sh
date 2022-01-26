003429ecebdc407ba50dfcbcdfbe531259410c2d
Oleg Fokin <oleg.fokin@harmonicinc.com>
2022-01-26 13:10:06 +0200

    config test

--- .clusterfuzzlite/build.sh
+++ .clusterfuzzlite/build.sh
@@ -1,7 +1,3 @@
 #!/bin/bash -eu
 
-echo "SRC: $SRC"
-echo "OUT: $OUT"
-echo "PWD: $PWD"
-
-$CXX $CXXFLAGS -std=c++17 $SRC/fuzz.cpp -o $OUT/fuzzz/fuzzer
+$CXX $CXXFLAGS -std=c++17 $SRC/fuzzz/fuzz.cpp -o $OUT/fuzzz/fuzzer
