746650c370c25e7c14ed1ecfe6eef9e1a9863e54
Oleg Fokin <oleg.fokin@harmonicinc.com>
2022-01-26 13:04:54 +0200

    config test

--- .clusterfuzzlite/build.sh
+++ .clusterfuzzlite/build.sh
@@ -1,16 +1,7 @@
 #!/bin/bash -eu
 
-# build project
-# e.g.
-# ./autogen.sh
-# ./configure
-# make -j$(nproc) all
+echo "SRC: $SRC"
+echo "OUT: $OUT"
+echo "PWD: $PWD"
 
-# build fuzzers
-# e.g.
-# $CXX $CXXFLAGS -std=c++11 -Iinclude \
-#     /path/to/name_of_fuzzer.cc -o $OUT/name_of_fuzzer \
-#     $LIB_FUZZING_ENGINE /path/to/library.a
-
-#$CXX $CXXFLAGS $SRC -o $OUT
-clang++ -std=c++17 -stdlib=libstdc++ -g -fsanitize=address,fuzzer fuzz.cpp -o fuzzer
+$CXX $CXXFLAGS -std=c++17 $SRC/fuzz.cpp -o $OUT/fuzzz/fuzzer
